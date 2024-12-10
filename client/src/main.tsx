bdca7bc873164d84c882bdde983b2306fc0c6ba9
LeopoldDePalma <k.konyushkin@icloud.com>
2024-12-10 15:28:04 +0300

    new dev

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './app/App.tsx'
-import { store } from './app/lib/store.ts'
-import { injectStore } from './shared/api/axiosInstance.ts'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './app/App.tsx';
+import { store } from './app/lib/store.ts';
+import { injectStore } from './shared/api/axiosInstance.ts';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
